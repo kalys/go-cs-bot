8fff920c878b95ae50ef201efcdc1d6f28d3e8d3
Kalys Osmonov <kalys@osmonov.com>
2018-01-08 15:31:24 +0600

    Add go routine that tracks users

--- main.go
+++ main.go
@@ -21,6 +21,8 @@ func main() {
 	if !ok {
 		fmt.Printf("%s not set\n", "CS_HOST")
 	}
+	address := fmt.Sprintf("%s:27015", cs_host)
+	// address = "46.42.18.18:27018"
 
 	b, err := tb.NewBot(tb.Settings{
 		Token:  token,
@@ -32,15 +34,64 @@ func main() {
 		return
 	}
 
-	// user := tb.User{ID: 62540640}
-	// b.Send(&user, "aoeuaoeuaoeua aoeuaoeu")
-	// return
+	go func() {
+		var prevPlayers []valve.Player
+		c := time.Tick(10 * time.Second)
+		for now := range c {
+			_ = now
+			client, err := valve.NewServerQuerier(address, 10*time.Second)
+			defer client.Close()
+			if err != nil {
+				continue
+			}
+
+			playersInfo, err := client.QueryPlayers()
+			if err != nil {
+				continue
+			}
+
+			new, left := playersInfo.Diff(prevPlayers)
+
+			prevPlayers = playersInfo.Players
+
+			var buffer bytes.Buffer
+			if len(new) > 0 {
+				for index, player := range new {
+					buffer.WriteString(player.Nickname)
+					if index < len(new)-1 {
+						buffer.WriteString(", ")
+					}
+				}
+				buffer.WriteString(" connected")
+			}
+
+			if len(left) > 0 {
+				if len(new) > 0 {
+					buffer.WriteString("\n")
+				}
+
+				for index, player := range left {
+					buffer.WriteString(player.Nickname)
+					if index < len(left)-1 {
+						buffer.WriteString(", ")
+					}
+				}
+				buffer.WriteString(" left")
+
+			}
+
+			if len(new) > 0 || len(left) > 0 {
+				chat := tb.Chat{ID: -38942708}
+				b.Send(&chat, buffer.String())
+			}
+
+		}
+	}()
 
 	b.Handle("/players", func(m *tb.Message) {
-		address := fmt.Sprintf("%s:27015", cs_host)
-		// address = "46.42.18.18:27018"
 		client, err := valve.NewServerQuerier(address, 10*time.Second)
 		defer client.Close()
+
 		if err != nil {
 			b.Send(m.Sender, "something wrong")
 			return
@@ -65,8 +116,6 @@ func main() {
 			}
 			table.Render()
 		}
-
-		fmt.Println(m.Sender)
 
 		response := fmt.Sprintf("```\nPlayers: %d\nBots: %d\nMap: %s%s```", info.Players, info.Bots, info.MapName, buffer.String())
 
